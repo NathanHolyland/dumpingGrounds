e1fa92e2e4677ef004475c6237cd9c14e9202f1d
Nathan Holyland <94959991+NathanHolyland@users.noreply.github.com>
2022-02-23 08:41:19 +0000

    Button Class Changes

--- Menu.py
+++ Menu.py
@@ -1,51 +1,60 @@
 import pygame
+from random import randint
 
 class Button:
-    def __init__(self, color, rect, function, toggleable, *args):
-        self.color = color
+    def __init__(self, bgColor, texture, rect, toggleable):
+        font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.texture = texture
+        self.color = bgColor
         self.rect = rect
-        self.action = function
-        self.args = args
         self.toggleable = toggleable
         self.state = False
-        
-    def draw(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect, 0)
+        self.visible = True
 
-    def update(self):
-        if self.state:
-            self.action(*self.args)
-        if not self.toggleable:
-            self.state = False
+    def draw(self, surface):
+        if self.visible:
+            pygame.draw.rect(surface, self.color, self.rect, 0)
+            surface.blit(self.texture, (self.rect[0], self.rect[1]))
 
     def check(self, mousePos):
         x, y, w, h = self.rect
-        if (x <= mousePos[0] <= x+w) and (y <= mousePos[1] <= y+h):
-            if self.toggleable:
-                self.state = not self.state
-            else:
-                self.state = True
+        if self.visible:
+            if (x <= mousePos[0] <= x+w) and (y <= mousePos[1] <= y+h):
+                if self.toggleable:
+                    self.state = not self.state
+                else:
+                    return True
+        return self.state
 
-def add(*args):
-    total = 0
-    for item in args:
-        total += item
-    print(total)
+def checkButtons(buttons, mousePos):
+    event_list = {}
+    for button in buttons:
+        event_list[button] = button.check(mousePos)
+    return event_list
 
 screen = pygame.display.set_mode([500, 500])
-b1 = Button((255, 0, 0), [50, 50, 20, 20], add, True, 5, 2, 6, 1051)
-
+pygame.font.init()
+comic = pygame.font.SysFont("Comic Sans MS", 30)
+b1 = Button((255, 0, 0), comic.render("Hello", False, (0, 0, 0)), [50, 50, 60, 20], False)
+b2 = Button((0, 0, 255), comic.render("Visibility", False, (0, 0, 0)), [50, 70, 100, 20], True)
+bList = [b1,b2]
 running = True
 while running:
+    screen.fill((0,0,0))
     b1.draw(screen)
+    b2.draw(screen)
     pygame.display.flip()
 
-    b1.update()
-
-    mousePos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
-            b1.check(mousePos)
+            button_events = checkButtons(bList, pygame.mouse.get_pos())
+            if button_events[b1]:
+                colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+                b1.color = colors[randint(0, 2)]
+                b1.visible = False
+            b1.visible = button_events[b2]
+
+
         
         if event.type == pygame.QUIT:
             running = False
